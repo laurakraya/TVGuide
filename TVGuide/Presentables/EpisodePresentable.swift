058e4feeaf6af20c6377d72ed5abf1c27b9d0854
Laura Krayacich <laura.krayacich@uala.com.ar>
2020-02-21 14:12:07 -0300

    EpisodePresentable

---
+++ TVGuide/Presentables/EpisodePresentable.swift
@@ -0,0 +1,9 @@
+//
+//  EpisodePresentable.swift
+//  TVGuide
+//
+//  Created by Laura Daniela Krayacich on 21/02/2020.
+//  Copyright © 2020 Laura Daniela Krayacich. All rights reserved.
+//
+
+import Foundation
