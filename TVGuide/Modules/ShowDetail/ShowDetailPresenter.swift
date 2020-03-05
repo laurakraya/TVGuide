5939067211ce9de54de34d6e823764f8a1973e52
Laura Krayacich <laura.krayacich@uala.com.ar>
2020-03-05 17:39:31 -0300

    print episodes title by season from episodePresentable array

--- TVGuide/Modules/ShowDetail/ShowDetailPresenter.swift
+++ TVGuide/Modules/ShowDetail/ShowDetailPresenter.swift
@@ -55,6 +55,25 @@ class ShowDetailPresenter {
         self.episodesPresentables = ep
         
     }
+    
+    func printEpisodesBySeason(_ episodes: [EpisodePresentable]) {
+        
+        let episodesBySeason = Dictionary(grouping: episodes, by: { $0.season! })
+        
+        let episodesBySeasonSorted = episodesBySeason.sorted { $0.0 < $1.0 } .map { $0 }
+        
+        episodesBySeasonSorted.forEach({
+            print("------------------ Season \($0.key) -----------------")
+
+            $0.value.forEach({
+                if let episodeName = $0.name {
+                    print(episodeName)
+                }
+            })
+        })
+        
+        //PARA MAÑANA: CONTENT SIZE OF TABLE VIEW PARA PROBLEMAS DE SCROLL
+    }
 
 }
 
@@ -63,6 +82,7 @@ extension ShowDetailPresenter: SDInteractorToSDPresenter {
     func didRespond(episodes: [Episode]) {
         
         EpisodestoEPresentables(episodes: episodes)
+        printEpisodesBySeason(self.episodesPresentables)
         view?.displayEpisodes(episodes: self.episodesPresentables)
         
     }
